471f0c3fe4a3bdc28d3615cc81ba2e661dd5c743
Matthias Heinzel <mheinzel@posteo.de>
2018-01-10 16:40:00 +0100

    ActorPi: create common re-export module

---
+++ implementation/experiments/src/ActorPi.hs
@@ -0,0 +1,9 @@
+module ActorPi (module X) where
+
+import ActorPi.Syntax    as X
+import ActorPi.Context   as X
+import ActorPi.TypeCheck as X
+import ActorPi.Show      as X hiding (showIn)
+import ActorPi.LaTeX     as X hiding (showIn)
+import ActorPi.Examples  as X
+import ActorPi.Scripts   as X
