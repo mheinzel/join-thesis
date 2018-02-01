b03c5845202713df603012830ad852aea3accece
Matthias Heinzel <mheinzel@posteo.de>
2018-02-01 17:47:17 +0000

    erlang: do all communication over gproc

--- implementation/join/src/join.erl
+++ implementation/join/src/join.erl
@@ -1,55 +1,101 @@
 -module(join).
--export([
-  def/2,
-  actor/5,
-  forward/2
-]).
-
-% TODO:
-% parallel
-% synchronous
-% polyadic
-
-% TODO: better solution for single channel def (no jon)
-simple_handler(P) ->
-  receive
-    U -> P(self(), U), simple_handler(P)
-  end.
-
-def(P, Q) when is_function(Q, 1) ->
-  X = spawn(fun() -> simple_handler(P) end),
-  spawn(fun() -> Q(X) end);
+-compile(export_all).
 
 
-def(P, Q) ->
-  A = spawn(?MODULE, actor, [P, x, y, queue:new(), queue:new()]),
-  X = spawn(?MODULE, forward, [x, A]),
-  Y = spawn(?MODULE, forward, [y, A]),
-  spawn(fun() -> Q(X, Y) end),
+-define(DEBUG(F, A), ok).
+%-define(DEBUG(Format, Args), io:format("~p:~p: " ++ Format ++ "~n", [?FUNCTION_NAME, ?LINE | Args])).
+
+% TODO
+% split out insert and join?
+% rename function names corresponing to actor definitions?
+
+
+
+% node() makes it globally unique
+get_id(Who) ->
+  {Who, node(), erlang:unique_integer()}.
+
+send(Channel, Payload) ->
+  ?DEBUG("sending ~p to ~p", [Payload, Channel]),
+  spawn(fun() ->
+            {Pid, _} = gproc:await({n, l, Channel}),  % might not be registered yet
+            Pid ! Payload
+        end).
+
+% spawn and register a behavior on a given channel
+% based on behavior syntax in ActorPi
+behavior(Bhv, Channel, Args) ->
+  ?DEBUG("starting on ~p", [Channel]),
+  spawn(fun() ->
+            gproc:reg({n, l, Channel}),
+            apply(Bhv, [Channel | Args])
+        end).
+
+
+% temporary, just to make it easier to define continuations
+% (could also be overloaded with regular def)
+def_single(PQ) ->
+  A = get_id(sng),
+  {P, Q} = PQ(A),
+  Actor = fun Single(_) ->
+      receive
+        U -> spawn(fun() -> P(U) end),
+             Single
+      end
+    end,
+  behavior(Actor, A, []),
+  spawn(Q),
   A.
 
-actor(P, X, Y, Us, Vs) ->
-  receive
-    {X, U} ->
-      case queue:out(Vs) of
-        {empty, _} -> actor(P, X, Y, queue:in(U, Us), Vs);
-        {{value, V}, T} ->
-          spawn(fun() -> P(X, U, Y, V) end),
-          actor(P, X, Y, Us, T)
-      end;
-    {Y, V} ->
-      case queue:out(Us) of
-        {empty, _} -> actor(P, X, Y, Us, [V | Vs]);
-        {{value, U}, T} ->
-          spawn(fun() -> P(X, U, Y, V) end),
-          actor(P, X, Y, T, Vs)
-      end;
-    status ->
-      io:format("Us: ~p~nVs: ~p~n", [Us, Vs]),
-      actor(P, X, Y, Us, Vs)
+def(PQ) ->
+  ?DEBUG("~p", [PQ]),
+  % new names
+  A = get_id(act),
+  X = get_id(fwX),
+  Y = get_id(fwY),
+  Lx = queue:new(),
+  Ly = queue:new(),
+  % get user-supplied processes
+  {P, Q} = PQ(X, Y),
+  % spawn in parallel (no need to spawn lists)
+  behavior(actor(P), A, [X, Y, Lx, Ly]),  % P passed at runtime only in implementation
+  behavior(fun forward/2, X, [A]),
+  behavior(fun forward/2, Y, [A]),
+  spawn(Q),
+  % return actor name just for debugging
+  A.
+
+
+% B_a
+actor(P) ->
+  fun Actor(A, X, Y, Us, Vs) ->
+    receive
+      {X, U} ->
+        case queue:out(Vs) of
+          {empty, _} -> Actor(A, X, Y, queue:in(U, Us), Vs);
+          {{value, V}, T} ->
+            spawn(fun() -> P(U, V) end),
+            Actor(A, X, Y, Us, T)
+        end;
+      {Y, V} ->
+        case queue:out(Us) of
+          {empty, _} -> Actor(A, X, Y, Us, queue:in(V, Vs));
+          {{value, U}, T} ->
+            spawn(fun() -> P(U, V) end),
+            Actor(A, X, Y, T, Vs)
+        end;
+      status ->
+        io:format("Us: ~p~nVs: ~p~n", [Us, Vs]),
+        Actor(A, X, Y, Us, Vs);
+      Other ->
+        io:format("warning: actor received invalid message: ~p~n", [Other]),
+        Actor(A, X, Y, Us, Vs)
+    end
   end.
 
+% B_{c}
 forward(X, A) ->
   receive
-    I -> A ! {X, I}, forward(X, A)
+    I -> send(A, {X, I}),
+         forward(X, A)
   end.
--- implementation/join/src/join_examples.erl
+++ implementation/join/src/join_examples.erl
@@ -3,11 +3,33 @@
 
 
 print() ->
-  join:def(fun(_X,U,_Y,V) -> io:format("joined ~p and ~p~n", [U, V]) end,
-      fun(X,Y) -> X ! "a", X ! "b", Y ! "1", Y ! "2", Y ! "3" end).
+  join:def(fun(X, Y) -> {
+    fun(U, V) ->
+      io:format("joined ~p and ~p~n", [U, V])
+    end,
+    % in
+    fun() ->
+      join:send(X, "a"),
+      join:send(X, "b"),
+      join:send(Y, "1"),
+      join:send(Y, "2"),
+      join:send(Y, "3")
+    end
+  } end).
 
 pi_channel() ->
-  join:def(fun(_S,X,_R,K) -> K ! X end, % "reply X to _R"
-      fun(Send,Receive) -> Send ! 1, Send ! 2,
-                           join:def(fun(_K,V) -> io:format("received ~p~n", [V]) end,
-                               fun(K) -> Receive ! K end) end).
+  join:def(fun(Send, Receive) -> {
+    fun(X, K) ->
+      join:send(K, X)
+    end,
+    % in
+    fun() ->
+      join:send(Send, 1),
+      join:send(Send, 2),
+      join:def_single(fun(K) -> {
+        fun(V) -> io:format("received ~p~n", [V]) end,
+        % in
+        fun() -> join:send(Receive, K) end
+      } end)
+    end
+  } end).
