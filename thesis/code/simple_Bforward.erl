df3e84671dc84179da47f2ed2bef24d3a7ae8e3b
Matthias Heinzel <mheinzel@posteo.de>
2018-03-05 11:58:02 +0000

    impl: simplify code of def

--- thesis/code/simple_Bforward.erl
+++ thesis/code/simple_Bforward.erl
@@ -1,5 +1,5 @@
 forward(X, A) ->
   receive
-    I -> join_reg:send(A, {X, I}),
+    I -> join:send(A, {X, I}),
          forward(X, A)
   end.
--- thesis/code/simple_def_encoding.erl
+++ thesis/code/simple_def_encoding.erl
@@ -1,14 +1,13 @@
-def(Location, PQ) ->
+def(PQ) ->
   % new names
-  A = join_util:create_id(act),
-  X = join_util:create_id(fwX),
-  Y = join_util:create_id(fwY),
+  A = join:create_id(act),
+  X = join:create_id(fwX),
+  Y = join:create_id(fwY),
   Payloads = [],
   % get user-supplied processes
   {P, Q} = PQ(X, Y),
   % spawn in parallel (no need to spawn lists)
-  join_location:spawn_actor_at(Location, join_actor:definition(P),
-                               A, [X, Y, X, Payloads]),
-  join_location:spawn_actor_at(Location, fun join_actor:forward/2, X, [A]),
-  join_location:spawn_actor_at(Location, fun join_actor:forward/2, Y, [A]),
+  join:spawn_actor(join_actor:definition(P), A, [X, Y, X, Payloads]),
+  join:spawn_actor(fun join_actor:forward/2, X, [A]),
+  join:spawn_actor(fun join_actor:forward/2, Y, [A]),
   spawn(Q).
