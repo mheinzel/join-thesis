760774be78a050e79e95bbc21cbd105f19f7037c
Matthias Heinzel <mheinzel@posteo.de>
2021-06-29 14:18:49 +0200

    make slides

--- thesis/implementation/code/simple/def_encoding.erl
+++ thesis/implementation/code/simple/def_encoding.erl
@@ -1,13 +1,15 @@
 def(PQ) ->
-  % new names
   A = join:create_id(act),
   X = join:create_id(fwX),
   Y = join:create_id(fwY),
   Payloads = [],
-  % get user-supplied processes
+
   {P, Q} = PQ(X, Y),
-  % spawn in parallel (no need to spawn lists)
-  join:spawn_actor(join_actor:definition(P), A, [X, Y, X, Payloads]),
-  join:spawn_actor(fun join_actor:forward/2, X, [A]),
-  join:spawn_actor(fun join_actor:forward/2, Y, [A]),
+
+  join:spawn_actor(join:definition(P),
+                   A, [X, Y, X, Payloads]),
+  join:spawn_actor(fun join:forward/2,
+                   X, [A]),
+  join:spawn_actor(fun join:forward/2,
+                   Y, [A]),
   spawn(Q).
